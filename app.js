1059abcb7c07365af74b869a421b09a988c97188
Max Braz <braz.max@gmail.com>
2017-03-06 13:28:21 -0800

    app.js, index.html, package.json, server.js, install mongo, express, mongoose, nodemon, basic html with styling, rendering upovtes

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = angular.module('phishly', []);
+
+app.controller('MainCtrl', [
+  '$scope',
+  function($scope) {
+    $scope.songs = [
+        {name: 'Bathtub Gin', url: 'http://phish.in/1997-08-17/bathtub-gin', upvotes: 0},
+        {name: 'Tweezer', url: 'http://phish.in/2013-07-31/tweezer', upvotes: 5},
+        {name: 'Roses are Free', url: 'http://phish.in/1998-04-03/roses-are-free', upvotes: 0},
+        {name: 'ACDC Bag', url: 'http://phish.in/1999-09-14/ac-dc-bag', upvotes: 2},
+        {name: 'Harry Hood', url: 'http://phish.in/1993-12-31/harry-hood', upvotes: 0},
+        {name: 'Chalkdust Torture', url: 'http://phish.in/1999-07-10/chalk-dust-torture', upvotes: 7},
+        {name: 'Prince Caspian', url: 'http://phish.in/2015-08-22/prince-caspian', upvotes: 0},
+        {name: 'Ghost', url: 'http://phish.in/2000-05-22/ghost', upvotes: 9},
+        {name: 'You Enjoy Myself', url: 'http://phish.in/1995-12-09/you-enjoy-myself', upvotes: 0}
+    ];
+    $scope.incrementUpvotes = function(song) {
+      song.upvotes += 1;
+    }
+  }
+]);
