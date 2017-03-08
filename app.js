a6c170205a030ab469cb16d0640f926895a08c19
Max Braz <braz.max@gmail.com>
2017-03-07 18:19:06 -0800

    implemented server connection and basic db setup. moved public files into the client directory

--- app.js
+++ app.js
@@ -1,56 +1,48 @@
-var app = angular.module('Phishly', ['ngSanitize']);
+var express = require('express');
+var path = require('path');
+var app = express();
+var mongoose = require('mongoose');
 
-// app.service('ZenService', [function($http) {
-//   this.getZen = function() {
-//     return $http({
-//       method: 'GET',
-//       url: 'https://api.github.com/zen'
-//     });
-//   };
-// }]);
+mongoose.connect('mongodb://localhost/songs');
 
-app.factory('songs', [function() {
-  var songList = {
-    songs: [
-      {name: 'Bathtub Gin', url: 'http://phish.in/1997-08-17/bathtub-gin', upvotes: 0},
-      {name: 'Tweezer', url: 'http://phish.in/2013-07-31/tweezer', upvotes: 5},
-      {name: 'Roses are Free', url: 'http://phish.in/1998-04-03/roses-are-free', upvotes: 0},
-      {name: 'ACDC Bag', url: 'http://phish.in/1999-09-14/ac-dc-bag', upvotes: 2},
-      {name: 'Harry Hood', url: 'http://phish.in/1993-12-31/harry-hood', upvotes: 0},
-      {name: 'Chalkdust Torture', url: 'http://phish.in/1999-07-10/chalk-dust-torture', upvotes: 7},
-      {name: 'Prince Caspian', url: 'http://phish.in/2015-08-22/prince-caspian', upvotes: 0},
-      {name: 'Ghost', url: 'http://phish.in/2000-05-22/ghost', upvotes: 9},
-      {name: 'You Enjoy Myself', url: 'http://phish.in/1995-12-09/you-enjoy-myself', upvotes: 0}
-    ]
-  }
-  return songList
-}]);
+var db = mongoose.connection;
 
-app.controller('MainCtrl', [
-  '$scope', '$sce', 'songs',
+var songSchema = mongoose.Schema({
+  name: String,
+  url: String,
+  upvotes: { type: String, default: 0 }
+});
 
-  function($scope, $sce, songs) {
-    $scope.trustSrc = function(src) {
-      return $sce.trustAsResourceUrl(src);
-    };
+var Song = mongoose.model('Song', songSchema);
 
-    $scope.songs = songs.songs;
+var brother = new Song({
+  name: 'Brother',
+  url: 'http://phish.in/1998-04-04/brother'
+});
 
-    $scope.incrementUpvotes = function(song) {
-      song.upvotes += 1;
-    };
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+});
 
-    $scope.iframeUrl = '';
+app.use(express.static(path.join(__dirname, 'client')));
 
-    $scope.iframeLoad = function(song) {
-      $scope.iframeUrl = song.url;
-    };
+// app.get('/', function(req, res) {
+//   // res.render('/index.html')
+//   res.sendFile('index.html', {root : __dirname + '/'});
+// });
+//
+// app.post('/', function(req, res) {
+//   res.send('Got a Post request');
+// });
+//
+// app.put('/user', function(req, res) {
+//   res.send('Got a PUT request at /user');
+// });
+//
+// app.delete('/user', function(req, res) {
+//   res.send('Got a DELETE request at /user');
+// });
 
-    $scope.addSong = function() {
-      if (!$scope.name || $scope.name === '') {return;}
-      $scope.songs.push({name: '$scope.name', url: '$scope.link', upvotes: 0})
-      $scope.name = '';
-      $scope.link = '';
-    };
-  }
-]);
+app.listen(3000, function() {
+  console.log('phishly is listening on port 3000')
+});
