0fe5197389c71b092c7175c59fe26345f03a4299
Max Braz <braz.max@gmail.com>
2017-04-09 15:07:08 -0700

    remove bower_components from git

--- client/angularApp.js
+++ client/angularApp.js
@@ -1,47 +1,57 @@
-var app = angular.module('Phishly', ['ngSanitize']);
+var app = angular.module('app', ['ngSanitize']);
 
-app.factory('songs', [function() {
-  var songList = {
-    songs: [
-      {name: 'Bathtub Gin', url: 'http://phish.in/1997-08-17/bathtub-gin', upvotes: 0},
-      {name: 'Tweezer', url: 'http://phish.in/2013-07-31/tweezer', upvotes: 5},
-      {name: 'Roses are Free', url: 'http://phish.in/1998-04-03/roses-are-free', upvotes: 0},
-      {name: 'ACDC Bag', url: 'http://phish.in/1999-09-14/ac-dc-bag', upvotes: 2},
-      {name: 'Harry Hood', url: 'http://phish.in/1993-12-31/harry-hood', upvotes: 0},
-      {name: 'Chalkdust Torture', url: 'http://phish.in/1999-07-10/chalk-dust-torture', upvotes: 7},
-      {name: 'Prince Caspian', url: 'http://phish.in/2015-08-22/prince-caspian', upvotes: 0},
-      {name: 'Ghost', url: 'http://phish.in/2000-05-22/ghost', upvotes: 9},
-      {name: 'You Enjoy Myself', url: 'http://phish.in/1995-12-09/you-enjoy-myself', upvotes: 0}
-    ]
-  }
-  return songList
-}]);
+app.controller('MainCtrl', function($scope, $sce, SongService) {
+    
+  $scope.songList = [];
 
-app.controller('MainCtrl', [
-  '$scope', '$sce', 'songs', '$http',
+  $scope.init = function() {
+    $scope.getSongs();
+  };
+  
+  $scope.trustSrc = function(src) {
+    return $sce.trustAsResourceUrl(src);
+  };
 
-  function($scope, $sce, songs, $http) {
-    $scope.trustSrc = function(src) {
-      return $sce.trustAsResourceUrl(src);
+  $scope.iframeUrl = '';
+
+  $scope.iframeLoad = function(song) {
+    $scope.iframeUrl = song.url;
+  };
+
+  // $scope.songs = songs.songs;
+
+  $scope.postSong = function(name, url) {
+    var data = {
+      name: name,
+      url: url
     };
 
-    $scope.songs = songs.songs;
+    SongService.post(data);
+  };
 
-    $scope.incrementUpvotes = function(song) {
-      song.upvotes += 1;
-    };
+  $scope.getSongs = function() {
+    SongService.get().then(function(data) {
+      $scope.songList = data.data;
+    });
+  };
+  $scope.incrementUpvotes = function(song) {
+    song.upvotes += 1;
+  };
+});
 
-    $scope.iframeUrl = '';
+app.service('SongService', function($http) {
+  this.post = function(data) {
+    return $http({
+      method: 'POST',
+      url: '/song',
+      data: data
+    });
+  };
 
-    $scope.iframeLoad = function(song) {
-      $scope.iframeUrl = song.url;
-    };
-
-    $scope.addSong = function() {
-      if (!$scope.name || $scope.name === '') {return;}
-      $scope.songs.push({name: '$scope.name', url: '$scope.link', upvotes: 0})
-      $scope.name = '';
-      $scope.link = '';
-    };
-  }
-]);
+  this.get = function() {
+    return $http({
+      method: 'GET',
+      url: '/songs'
+    });
+  };
+});
